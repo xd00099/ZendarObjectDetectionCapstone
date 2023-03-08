e759f0b5ace6f02501cd8554c24205989f11b745
Constantin1709 <71826191+Constantin1709@users.noreply.github.com>
2023-03-08 10:02:02 -0800

    yolo implemented

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from src.data.dataset import Scene, CameraRadarProps
 from src.radar_to_image import single_radar_to_img, multi_radar_to_img
+from src.model.yolo import run_yolo
+import matplotlib.pyplot as plt
 
 LEFT_CAM = '21248038'
 MID_CAM = '20438665'
@@ -12,6 +14,10 @@ RIGHT_RADAR = 'ZRVE1001'
 filepath = 'data/00000.npz'
 sensor_prop_path = 'data/extrinsics_intrinsics.npz'
 
+yolo_cfg_path = 'yolo_setup/yolov3.cfg'
+yolo_class_path = 'yolo_setup/yolov3.txt'
+yolo_weights_path = 'yolo_setup/yolov3.weights'
+
 if __name__ == '__main__':
 
     data = Scene(filepath)
@@ -20,6 +26,11 @@ if __name__ == '__main__':
     radar_data = data.get_radar_data()
     camera_data = data.get_camera_data()
     cam = MID_CAM
+
+    lab_img, bb = run_yolo(camera_data[RIGHT_CAM], yolo_cfg_path, yolo_class_path, yolo_weights_path)
+
+    plt.imshow(lab_img[0])
+    plt.show()
 
     multi_radar_to_img(radar_data, sensor_props.radar_extrinsic, 
                        sensor_props.camera_extrinsics[cam], sensor_props.camera_intrinsics[cam], image=camera_data[cam])
---
+++ src/model/yolo.py
@@ -0,0 +1,122 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+
+
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    try:
+        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    except:
+        output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h, classes, COLORS):
+    label = str(classes[class_id])
+
+    color = COLORS[class_id]
+
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    plt.imshow(img)
+    plt.show()
+
+
+
+def run_yolo(img, cfg_path, class_path, weights_path):
+
+    # Model Setup
+    config_path = cfg_path
+    classes_path = class_path
+    weights_path = weights_path
+
+    # Classes Path
+    classes = None
+    with open(classes_path, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    # Config
+    net = cv2.dnn.readNet(weights_path, config_path)
+
+
+    # Results
+    labeled_images = []
+    image_boxes_np = []
+    box_coordinates = []
+
+    # Pass Image
+    image = img
+    plt.imshow(image)
+    plt.show()
+
+    # Blob
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        try:
+            box = boxes[i]
+        except:
+            i = i[0]
+            box = boxes[i]
+
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+
+        # Remove Classes that are not
+        if class_ids[i] == 0 or class_ids[i] == 2 or class_ids[i] == 5 or class_ids[i] == 7:
+             draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h), classes, COLORS)
+
+        box_coordinates.append(box)
+
+
+    image_boxes_np.append(box_coordinates)
+    labeled_images.append(image)
+
+
+    image_boxes_np = np.array(image_boxes_np, dtype=object)
+
+    return labeled_images, image_boxes_np
+
