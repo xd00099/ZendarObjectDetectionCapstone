88aac10ecad3b055cdc95b3da386196be57b3c90
xd00099 <dxiang@ucsd.edu>
2023-03-15 15:22:19 -0700

    merged

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from src.data.dataset import Scene, CameraRadarProps
 from src.radar_to_image import single_radar_to_img, multi_radar_to_img
-from src.model.yolo import run_yolo
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
 
 LEFT_CAM = '21248038'
 MID_CAM = '20438665'
@@ -11,12 +12,13 @@ LEFT_RADAR = 'ZRVE1002'
 MID_RADAR = 'ZRVC2001'
 RIGHT_RADAR = 'ZRVE1001'
 
-filepath = 'data/00000.npz'
+filepath = 'data/00100.npz'
 sensor_prop_path = 'data/extrinsics_intrinsics.npz'
 
 yolo_cfg_path = 'yolo_setup/yolov3.cfg'
 yolo_class_path = 'yolo_setup/yolov3.txt'
 yolo_weights_path = 'yolo_setup/yolov3.weights'
+labeled_image = 'data/00000.npz_object-detection.jpg'
 
 if __name__ == '__main__':
 
@@ -25,7 +27,20 @@ if __name__ == '__main__':
 
     radar_data = data.get_radar_data()
     camera_data = data.get_camera_data()
-    cam = MID_CAM
+    cam = RIGHT_CAM
+
+    # camera_data[cam] -- middle image  --> bounding boxes
+    # plot boxes [(x1,y1,x2,y2)] x,y  x1 <  x < x2 , y1 < y < y2 
+    
+    labeled_image_toplot = mpimg.imread(labeled_image)
+    plt.figure(figsize=(20,10))
+
+    img = camera_data[cam]
+    # img = labeled_image_toplot
+
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
 
     lab_img, bb = run_yolo(camera_data[RIGHT_CAM], yolo_cfg_path, yolo_class_path, yolo_weights_path)
 
@@ -33,4 +48,6 @@ if __name__ == '__main__':
     plt.show()
 
     multi_radar_to_img(radar_data, sensor_props.radar_extrinsic, 
-                       sensor_props.camera_extrinsics[cam], sensor_props.camera_intrinsics[cam], image=camera_data[cam])
+                    sensor_props.camera_extrinsics[cam], sensor_props.camera_intrinsics[cam], image=img)
+
+    plt.savefig(f"view_100_right.png", dpi=200)
--- src/radar_to_image.py
+++ src/radar_to_image.py
@@ -17,8 +17,6 @@ def keep_inbound(image_coor, x_lim, y_lim):
     return np.array([[i,j] for i,j in image_coor if 0<i<x_lim and 0<j<y_lim])
 
 def multi_radar_to_img(radar_pts, radar_extrinsics, camera_extrinsics, camera_intrinsics, x_lim=1616, y_lim=1240, image=None):
-    plt.figure(figsize=(20,10))
-    plt.imshow(image)
 
     all_radar = {}
     for id, radar_pt in radar_pts.items():
@@ -27,12 +25,10 @@ def multi_radar_to_img(radar_pts, radar_extrinsics, camera_extrinsics, camera_in
         all_radar[id] = filtered_coor
     
         if image is not None:
-            plt.scatter(filtered_coor[:, 0], filtered_coor[:,1], label=id)
-    
-    plt.legend()
+            plt.scatter(filtered_coor[:, 0], filtered_coor[:,1], label=id, s=1.5)
+            
     if image is not None:
-        plt.show()
-
+        plt.legend()
 
     return all_radar
 
